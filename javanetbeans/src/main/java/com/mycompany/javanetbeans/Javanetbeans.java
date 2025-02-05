8e5c2569319b248fd1af3e3fc0524f3d36da6d59
Dave <Dave@DESKTOP-FKGJO7S.mshome.net>
2025-02-05 17:48:32 +0300

    variables and literals putting them together (java)

---
+++ javanetbeans/src/main/java/com/mycompany/javanetbeans/Javanetbeans.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javanetbeans;
+
+/**
+ *
+ * @author Dave
+ */
+public class Javanetbeans {
+
+    public static void main(String[] args) {
+        //Declaring and initializing variables
+        int age = 20;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isStudent = true;
+                
+        //printing the values
+        System.out.println("Age:" + age);
+        System.out.println("Price:" + price);
+        System.out.println("Grade:" + grade);
+        System.out.println("Is Student:" + isStudent);
+        
+        
+                
+    }
+}
