85088baaf5761c4c445d8ad24b5f736531ee7aa3
Dave <Dave@DESKTOP-FKGJO7S.mshome.net>
2025-02-12 15:58:59 +0300

    working with variables

---
+++ practicle1/src/main/java/com/mycompany/practicle1/Practicle1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicle1;
+
+/**
+ *
+ * @author Dave
+ */
+public class Practicle1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int apples = 10;
+        double pricePerApple = 0.75;
+                
+        double totalCost = apples + pricePerApple;
+        System.out.println("Total cost: " + totalCost);
+                
+                
+    }
+}
