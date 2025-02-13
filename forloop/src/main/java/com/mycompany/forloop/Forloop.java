630a1b4a5cfdeb3ce140d57682460b446beb6f0e
Dave <Dave@DESKTOP-FKGJO7S.mshome.net>
2025-02-13 11:07:50 +0300

    practice for the looping structure for loops

---
+++ forloop/src/main/java/com/mycompany/forloop/Forloop.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.forloop;
+import java.util.Scanner;
+/**
+ *
+ * @author Dave
+ */
+public class Forloop {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+Scanner input = new Scanner(System.in);
+int Mark;
+for (int x=0; x < 5; x++){
+    System.out.println("Enter marks for five student");
+    Mark = input.nextInt();
+}
+int Order;
+for (int  x=0; x < 10; x++){
+        System.out.println("First order:");
+        Order = input.nextInt();
+    }
+
+
+        
+
+
+
+    }
+}
