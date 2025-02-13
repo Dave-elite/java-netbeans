0c090b7e4c55234c2abc47234d70d5929bde9bdc
Dave <Dave@DESKTOP-FKGJO7S.mshome.net>
2025-02-13 09:27:27 +0300

    practice for control structures

---
+++ control-structures/src/main/java/com/mycompany/control/structures/ControlStructures.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.control.structures;
+
+/**
+ *
+ * @author Dave
+ */
+public class ControlStructures {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+        int mark =75;
+        if(mark >= 70){
+            System.out.println("Grade: A");
+        }else {
+            System.out.println("Failed");
+        }
+        
+    }
+}
