0d81ddf012c3a4b38eecbad472c16c0f5be4a4fa
Dave <Dave@DESKTOP-FKGJO7S.mshome.net>
2025-02-13 09:53:33 +0300

    practice for control structures if else statements

--- control-structures/src/main/java/com/mycompany/control/structures/ControlStructures.java
+++ control-structures/src/main/java/com/mycompany/control/structures/ControlStructures.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.control.structures;
-
+import java.util.Scanner;
 /**
  *
  * @author Dave
@@ -12,12 +12,26 @@ public class ControlStructures {
 
     public static void main(String[] args) {
 //        System.out.println("Hello World!");
-        int mark =75;
+Scanner scanner = new Scanner(System.in);
+//Ask the user to input the marks
+System.out.println("Enter the average mark: ");
+int mark = scanner.nextInt();
+        
         if(mark >= 70){
             System.out.println("Grade: A");
-        }else {
+        }else if(mark >= 60){
+            System.out.println("Grade: B");
+        }else if(mark >= 50){
+            System.out.println("Grade: C");
+        }else if(mark >= 40){
+            System.out.println("Grade: D");
+        }else if(mark >= 30){
             System.out.println("Failed");
+        }else{
+            System.out.println("Retake");
         }
         
+        //close the scanner object to avoid memeory leaks
+        scanner.close();
     }
 }
